6929aad2c69a8b5cec82ec890372f3e034f1f057
Domin <Your name>
2021-06-09 14:34:38 +0200

    Initial commit

---
+++ src/test/java/com/example/dodawanie/DodawanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dodawanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodawanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
